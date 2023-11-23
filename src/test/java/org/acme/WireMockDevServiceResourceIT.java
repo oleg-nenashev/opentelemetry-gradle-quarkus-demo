b6f693e80dcc307f2f751fe418074dfad93b1e0a
Oleg Nenashev <o.v.nenashev@gmail.com>
2023-11-23 22:37:52 +0100

    Add initial WireMock resource and Dev Service IT

---
+++ src/test/java/org/acme/WireMockDevServiceResourceIT.java
@@ -0,0 +1,8 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class WireMockDevServiceResourceIT extends WireMockDevServiceResourceTest {
+    // re-use Quarkus tests as integration tests
+}
---
+++ src/test/java/org/acme/WireMockDevServiceResourceTest.java
@@ -0,0 +1,31 @@
+package org.acme;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkiverse.wiremock.devservice.ConnectWireMock;
+
+@QuarkusTest
+@ConnectWireMock
+class WireMockDevServiceResourceTest {
+
+    private static final String MOCK_MSG = "Hello from WireMock!";
+    WireMock wiremock; // will be injected automatically when the class has been annotated with @ConnectWireMock
+
+    @Test
+    void testHelloEndpoint() {
+        Assertions.assertNotNull(wiremock);
+        wiremock.register(get(urlEqualTo("/mock-me")).willReturn(aResponse().withStatus(200).withBody(MOCK_MSG)));
+
+        given().when().get("/hello").then().statusCode(200).body(is(MOCK_MSG));
+    }
+}
