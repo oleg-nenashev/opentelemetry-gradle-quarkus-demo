2b469d412969e524bd2b459c01430aaa03baf9d9
Oleg Nenashev <onenashev@gradle.com>
2024-03-11 14:07:47 +0100

    Disable integration tests for now

--- src/test/java/org/acme/GreetingResourceIT.java
+++ src/test/java/org/acme/GreetingResourceIT.java
@@ -1,8 +1,10 @@
 package org.acme;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Disabled;
 
 @QuarkusIntegrationTest
+@Disabled
 public class GreetingResourceIT extends GreetingResourceTest {
     // Execute the same tests but in packaged mode.
 }
--- src/test/java/org/acme/WireMockDevServiceResourceIT.java
+++ src/test/java/org/acme/WireMockDevServiceResourceIT.java
@@ -1,8 +1,10 @@
 package org.acme;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Disabled;
 
 @QuarkusIntegrationTest
+@Disabled
 public class WireMockDevServiceResourceIT extends WireMockDevServiceResourceTest {
     // re-use Quarkus tests as integration tests
 }
