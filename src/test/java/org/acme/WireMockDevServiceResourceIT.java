109b203b7dd039db603c1ca677a66767e01202d7
Oleg Nenashev <onenashev@gradle.com>
2024-03-11 14:15:28 +0100

    Specify service destination

--- src/test/java/org/acme/WireMockDevServiceResourceIT.java
+++ src/test/java/org/acme/WireMockDevServiceResourceIT.java
@@ -3,6 +3,7 @@ package org.acme;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 import org.junit.jupiter.api.Disabled;
 
+// FIXME: https://github.com/quarkiverse/quarkus-wiremock/issues/106
 @QuarkusIntegrationTest
 @Disabled
 public class WireMockDevServiceResourceIT extends WireMockDevServiceResourceTest {
